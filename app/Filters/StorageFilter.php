1a38da79c948d78ad2ff0c7613bf9adffabdbb17
Thiago Bittencourt <thiago@ekkos.tech>
2023-11-06 22:40:56 -0300

    creating setMaxSize() in StorageFilter

--- app/Filters/StorageFilter.php
+++ app/Filters/StorageFilter.php
@@ -8,27 +8,35 @@ class StorageFilter implements FilterInterface
 {
     use ConversionTrait;
 
-    public function __construct(private readonly string $maxSize)
+    private readonly int|float $maxSize;
+
+    public function __construct(string $maxSize)
     {
+        $this->setMaxSize($maxSize);
     }
 
-    public function filter($item): bool
+    private function setMaxSize(string $maxSize): void
     {
-        // format expected: '2TB'
+        // expected format: '2TB'
         $pattern = '/(\d+)(TB|GB|MB|KB|B)([A-Z0-9]+)?/';
 
-        if (!preg_match($pattern, $this->maxSize, $matches)) {
-            return false;
+        if (!preg_match($pattern, $maxSize, $matches)) {
+            $this->maxSize = 0;
+
+            return;
         }
 
         $quantity = 1;
         $size = (int)$matches[1];
         $unit = $matches[2];
 
-        // convert sizes to GB for comparison
-        $maxSizeInGB = $this->convertToGB($quantity, $size, $unit);
+        // convert to GB for comparison
+        $this->maxSize = $this->convertToGB($quantity, $size, $unit);
+    }
 
-        // format expected: '4x480GBSSD'
+    public function filter($item): bool
+    {
+        // expected format: '4x480GBSSD'
         $pattern = '/(\d+)x(\d+)(TB|GB|MB|KB|B)([A-Z0-9]+)?/';
 
         if (!preg_match($pattern, $item[2], $matches)) {
@@ -39,9 +47,9 @@ class StorageFilter implements FilterInterface
         $size = (int)$matches[2];
         $unit = $matches[3];
 
-        // convert sizes to GB for comparison
+        // convert to GB for comparison
         $storageSize = $this->convertToGB($quantity, $size, $unit);
 
-        return $storageSize <= $maxSizeInGB;
+        return $storageSize <= $this->maxSize;
     }
 }
